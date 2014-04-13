32f6e6675571282d76a3e46b07f2dd73765bb397
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-04-13 16:48:26 +0200

    add gulp-rename and gulp-inject to devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,40 @@
 
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
+    rename = require('gulp-rename'),
+    inject = require('gulp-inject'),
     clean = require('gulp-clean');
 
-/**
- *
- * Clean Task
- *
- */
-gulp.task('clean', function() {
-    return gulp.src(['dist'], {read: false})
-        .pipe(clean());
-});
+var CONFIG = {
+    SRC: './src',
+    DIST: './dist'
+};
 
 /**
  *
  * Default Task
  *
  */
-gulp.task('default', ['clean']);
+gulp.task('default', ['clean', 'bundle-scripts']);
+
+/**
+ *
+ * Clean Task
+ *
+ */
+gulp.task('clean', function () {
+    return gulp.src([CONFIG.DIST], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('bundle-scripts', function () {
+    var browserifyBundle = gulp.src(CONFIG.SRC + '/app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest(CONFIG.DIST))
+        .pipe(rename('../app.js'));
+
+    return gulp.src(CONFIG.SRC + '/index.tpl.html')
+        .pipe(inject(browserifyBundle, { addRootSlash: false }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest(CONFIG.DIST));
+});
