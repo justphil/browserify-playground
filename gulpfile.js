2a1500d371b0c16578e3791b34fb79236b4aa5a2
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-04-13 14:06:25 +0200

    add gulp and several gulp plugins to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,22 @@
 "use strict";
+
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    clean = require('gulp-clean');
+
+/**
+ *
+ * Clean Task
+ *
+ */
+gulp.task('clean', function() {
+    return gulp.src(['dist'], {read: false})
+        .pipe(clean());
+});
+
+/**
+ *
+ * Default Task
+ *
+ */
+gulp.task('default', ['clean']);
